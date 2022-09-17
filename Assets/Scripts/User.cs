3d0c821eb65fbfa146b92c96b37e6e0ea43ce626
mine-cpp <paul0eiry0@gmail.com>
2022-09-16 23:14:49 -0300

    Added Basic Scripts and Font

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public static int english = 0,
+        portuguese = 1,
+        japanese = 2;
+    public int language = english;
+}
